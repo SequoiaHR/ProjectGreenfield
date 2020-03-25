da55fc58d00f3221c06b40a7a93e3808b2e8bdbe
Daniel <dprevoznik@gmail.com>
2020-03-25 08:49:20 -0400

    Added redux thunk middleware and fantastic logger function that allows quick access to previous & current state along with which action just occured.

--- src/redux/actionCreators/addDummyActionCreator.js
+++ src/redux/actionCreators/addDummyActionCreator.js
@@ -1,8 +1,7 @@
 import {ADD_DUMMY} from './actionTypes.js';
-import store from "../store.js"; // DO NOT NORMALLY LOAD STORE <- DONE FOR DEMONSTRATION PURPOSES
+// import store from "../store.js"; // DO NOT NORMALLY LOAD STORE <- DONE FOR DEMONSTRATION PURPOSES
 
 function addToArrayDummy(text) {
-    console.log('Current State before Action is Created: ', store.getState());
     return {
         type: ADD_DUMMY,
         text: text
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,17 @@
-import { createStore } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer.js";
-// add root reducer that combines other reducers here
-const store = createStore(rootReducer); //<- use it here
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        thunkMiddleware, // lets us dispatch() (async) functions
+        loggerMiddleware // neat middleware that logs actions
+    )
+); //<- use it here
 
 export default store;
