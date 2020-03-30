4d98f5f5a48a14f266f6c45dffadbd18d5373a06
Tristan Deitzer <tristandeitzer@gmail.com>
2020-03-30 17:20:17 -0400

    Add Answer Button and functionality

--- src/components/questions/questionsList.js
+++ src/components/questions/questionsList.js
@@ -16,8 +16,7 @@ constructor(props){
 
   //FETCHING INITIAL QUESTION DATA FROM STORE
   componentDidMount(){
-    console.log(this.props)
-    this.props.getProductQuestions(110);
+    this.props.getProductQuestions(this.props.paramsId);
   }
 
   //DETERMINES THE NUMBER OF QUESTIONS RENDERED
@@ -40,19 +39,22 @@ constructor(props){
   render(){
     return (
     <React.Fragment>
-      <h1 className="title">QUESTONS & ANSWERS</h1>
+      <h1 className="title">QUESTIONS & ANSWERS</h1>
       <div className="container questionsExpand">
         {
         (this.props.store.questions[0] ?
           this.determineDisplayed(this.props.store.questions).map( (question, index) => {
           return (
           <Question key={index}
+                    question_id={question.question_id}
                     question_body={question.question_body}
                     question_date={formatDate(question.question_date)}
                     asker_name={question.asker_name}
                     question_helpfulness={question.question_helpfulness}
                     reported={question.reported}
                     answers={question.answers}
+                    getProductQuestions={this.props.getProductQuestions}
+                    paramsId={this.props.paramsId}
           />)
         }) : <div>{" "}</div>)
         }
