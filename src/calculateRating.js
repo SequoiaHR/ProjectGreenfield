ebc9069d48b1ba95fbb4280ce17251b0b74e57de
Mayanne Chess <mayplusmay@gmail.com>
2020-03-30 20:18:25 -0400

    Change calculateRating back to returning a string for display purposes on whole numbers. Fix bugs in percent and progress bar displays in reviews breakdown. Update reviewsList component to re-render on URL change.

--- src/calculateRating.js
+++ src/calculateRating.js
@@ -17,7 +17,7 @@ const calculateRating = function(metadata) {
 
   const overallRating = (totalStars / reviewers).toFixed(1);
 
-  return Number(overallRating);
+  return overallRating;
 }
 
 export default calculateRating;
