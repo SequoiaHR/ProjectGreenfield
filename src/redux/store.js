a50eff191ef4b637810049827d46f4f078c929e8
Ryan Fulmer <rfulmer20@gmail.com>
2020-04-02 11:05:49 -0400

    changed middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,13 +3,13 @@ import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './rootReducer.js';
 
-const loggerMiddleware = createLogger();
+// const loggerMiddleware = createLogger();
 
 const store = createStore(
   rootReducer,
   applyMiddleware(
-    thunkMiddleware, // lets us dispatch() (async) functions
-    loggerMiddleware // neat middleware that logs actions
+    thunkMiddleware // lets us dispatch() (async) functions
+    // loggerMiddleware // neat middleware that logs actions
   )
 );
 
