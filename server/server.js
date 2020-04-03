abc0aa11f6d4e575d102edfe42541f559c73ccb1
Tristan Deitzer <tristandeitzer@gmail.com>
2020-04-03 14:19:45 -0400

    Quick update

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.get('/product/1', (req, res)=>{
-  res.sendFile(path.join(__dirname, "../build", "../build/index.html"))
+app.get('/*', (req, res)=>{
+  res.sendFile(path.join(__dirname, "../build/index.html"))
 })
 
 app.listen(80, () => {
