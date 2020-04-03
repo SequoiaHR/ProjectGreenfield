40151536399e0b08ed60540d27843dcc27da96ae
Daniel <dprevoznik@gmail.com>
2020-04-03 13:43:41 -0400

    Added changes to server file to allow correct build mode.

--- server/server.js
+++ server/server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 app.listen(3001, () => {
   console.log("*** Listening On Port 3001 ***");
