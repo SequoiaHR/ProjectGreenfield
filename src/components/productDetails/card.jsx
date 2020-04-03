93e174f641e0737f63dbd077037e337e8430f1ea
Daniel <dprevoznik@gmail.com>
2020-04-03 11:59:21 -0400

    Fixed class to className problems.

--- src/components/productDetails/card.jsx
+++ src/components/productDetails/card.jsx
@@ -89,12 +89,12 @@ const Card = ({
           >
             {listName === "Outfit" ? (
               <span className="icon is-medium fa-layers fa-fw">
-                <i style={{color: "white"}} class="fas fa-circle"></i>
+                <i style={{color: "white"}} className="fas fa-circle"></i>
                 <i className="fas fa-lg fa-times-circle has-text-danger"></i>
               </span>
             ) : (
               <span className="icon is-medium fa-layers fa-fw">
-                <i style={{color: "white"}}class="fas fa-star"></i>
+                <i style={{color: "white"}}className="fas fa-star"></i>
                 <i className="far fa-lg fa-star"></i>
               </span>
             )}
@@ -102,7 +102,7 @@ const Card = ({
 
           <AttachProductLink productId={product.id}>
             <img
-              class={`image-${listName}-product-${product.id}`}
+              className={`image-${listName}-product-${product.id}`}
               name={product.id}
               onClick={() => {
                 recordInteraction(
