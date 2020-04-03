8aad79b6430a0047fb5fce8e4775ab086b090e7c
Tristan Deitzer <tristandeitzer@gmail.com>
2020-04-03 13:53:43 -0400

    final demo fixes & deployment

--- src/components/questions/addQuestion.js
+++ src/components/questions/addQuestion.js
@@ -169,7 +169,7 @@ class AddQuestion extends React.Component {
         )}
         <div>For Authentication Reasons, You Will Not Be Emailed</div>
         <br />
-        <button className="button is-medium is-primary is-outlined " onClick={this.submitAnswer}>
+        <button className="button is-small is-primary is-outlined " onClick={this.submitAnswer}>
           SUBMIT QUESTION
         </button>
         <br />
